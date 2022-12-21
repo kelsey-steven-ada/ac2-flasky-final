921084ab063ee82a9599a745c21f05c1e0a58880
Ilana <117484277+ilana-adadev@users.noreply.github.com>
2022-12-21 10:26:12 -0800

    Update cat_routes.py
    updating validate_id_and_return_cat() to actually use SQLAlchemy to talk to the database for actual data.
    
    Tested via Postman!

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -37,20 +37,30 @@ def get_all_cats():
 
     return jsonify(cat_response)
 
-# @cats_bp.route("/<cat_id>", methods=["GET"])
-# def get_cat_by_id(cat_id):
-#     cat_to_return = validate_id_and_return_cat(cat_id) 
-#     return jsonify(cat_to_return.to_dict())
+@cats_bp.route("/<cat_id>", methods=["GET"])
+def get_cat_by_id(cat_id):
+    cat_to_return = validate_id_and_return_cat(cat_id)
+    cat_response = []
+    cat_response.append({
+        "id": cat_to_return.id,
+        "name": cat_to_return.name,
+        "breed": cat_to_return.breed,
+        "color": cat_to_return.color,
+        "size": cat_to_return.size,
+        "likes_catnip": cat_to_return.likes_catnip,
+    })
+    return jsonify(cat_response)
 
-# def validate_id_and_return_cat(cat_id):
-#     try:
-#         cat_id_as_int = int(cat_id)
-#     except:
-#         msg = f"Cat's id {cat_id} is not an integer"
-#         abort(make_response({"message": msg}, 400))
+def validate_id_and_return_cat(cat_id):
+    try:
+        cat_id_as_int = int(cat_id)
+    except:
+        msg = f"Cat's id {cat_id} is not an integer"
+        abort(make_response({"message": msg}, 400))
 
-#     for cat in cats:
-#         if cat.id == cat_id_as_int:
-#             return cat
+
+    cat = Cat.query.get(cat_id_as_int)
+    if cat.id == cat_id_as_int:
+        return cat
     
-#     abort(make_response({"message": f"Cat with id {cat_id} not found"}, 404))
+    abort(make_response({"message": f"Cat with id {cat_id} not found"}, 404))
