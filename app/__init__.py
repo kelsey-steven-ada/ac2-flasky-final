1de38eba1d0e5db6cbc6f3b87fa73cdc01a1877d
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2023-02-01 14:57:57 -0800

    Add CORS to app __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os 
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -31,5 +32,7 @@ def create_app(test_config=None):
 
     from .routes.caretaker_routes import caretakers_bp
     app.register_blueprint(caretakers_bp)
+
+    CORS(app)
 
     return app
